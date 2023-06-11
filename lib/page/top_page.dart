d1988c91ab096abe124d6b88ac9cf10deffcdae5
Genmai <hotaru103bsb@gmail.com>
2023-06-11 16:09:50 +0900

    refactor : APIを叩く処理をリファクタリング。 例外処理を追加。

--- lib/page/top_page.dart
+++ lib/page/top_page.dart
@@ -11,6 +11,8 @@ class TopPage extends StatefulWidget {
 
 class _TopPageState extends State<TopPage> {
   var imagePath = '';
+  String notFoundUrl =
+      'https://images.unsplash.com/photo-1584824486509-112e4181ff6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80';
 
   @override
   Widget build(BuildContext context) {
@@ -22,28 +24,33 @@ class _TopPageState extends State<TopPage> {
           ),
           CupertinoButton.filled(
             child: const Text('芝犬と一緒'),
-            onPressed: () {
-              _fetchShiba().then((value) {
-                if (value.statusCode != 200) {
-                  // throw Exception('データの取得にしっぱ')
-                  return print('データの取得に失敗しました...');
-                }
-                imagePath = jsonDecode(value.body)[0];
-                print('---------------------');
-                print(imagePath);
-                print('---------------------');
-              });
+            onPressed: () async {
+              try {
+                var path = await _fetchShiba();
+                setState(() {
+                  imagePath = path;
+                });
+              } catch (e) {
+                print(e);
+              }
             },
           ),
-          imagePath.isEmpty ? Text('ボタン押してね！') : Image.network(imagePath),
+          Image.network(imagePath.isNotEmpty ? imagePath : notFoundUrl),
         ],
       ),
     );
   }
 
-  Future<http.Response> _fetchShiba() {
+  // 芝犬APIを叩く関数
+  Future<String> _fetchShiba() async {
     String _url =
         'http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true';
-    return http.get(Uri.parse(_url));
+    var response = await http.get(Uri.parse(_url));
+
+    if (response.statusCode != 200) {
+      throw Exception('データの取得にしっぱいしました...');
+    }
+
+    return jsonDecode(response.body)[0];
   }
 }
